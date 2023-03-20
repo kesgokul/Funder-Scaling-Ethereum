730d5ee7f3a89f22dac6656bdc523503e3296b17
kesgokul <gokulkesavan5005@gmail.com>
2023-03-20 13:19:20 +0530

    bg waves

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        waves: "url(/images/bg-waves.png)",
+      },
+      backgroundSize: {
+        150: "150%",
+      },
+    },
+  },
+  plugins: [],
+};
